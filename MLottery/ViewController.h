579893ee44335efe3d5b5785e228e7a4202d261f
Jack <dalonng@gmail.com>
2015-04-13 14:04:14 +0800

    Initial Commit

---
+++ MLottery/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MLottery
+//
+//  Created by Jack on 4/13/15.
+//  Copyright (c) 2015 Jack. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ MLottery/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  MLottery
+//
+//  Created by Jack on 4/13/15.
+//  Copyright (c) 2015 Jack. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
